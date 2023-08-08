4de9900f97cc25b61a824ae5d98f3d39ea44d453
Muramori71 <dannyroh2500@gmail.com>
2023-08-07 18:36:41 -0700

    added pwa folder

---
+++ 19-PWA/01-Activities/17-Ins_Caching-Images/client/src/index.js
@@ -0,0 +1,12 @@
+import './css/style.css';
+
+import header from './js/header';
+
+// Register service worker
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
